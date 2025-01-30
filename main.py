6121233bc311d35e6358e4ea22ad4896c66de964
aclem43 <aclemeur@gmail.com>
2025-01-30 14:25:09 +1000

    Further improvments

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
 from st7735 import TFT
 from st7735 import sysfont
-from machine import SPI,Pin
+from machine import SPI,Pin,ADC, 
+import network
 import time
 import math
 
-def tftprinttest(font):
-    tft.fill(TFT.BLACK);
-    v = 30
-    tft.text((0, v), "Huon", TFT.RED, font, 1, nowrap=True)
-    v += font["Height"]
-    tft.text((0, v), "is", TFT.YELLOW, font, 2, nowrap=True)
-    v += font["Height"] * 2
-    tft.text((0, v), "Gay!", TFT.GREEN, font, 5, nowrap=True)
-    v += font["Height"] * 3
-    tft.text((0, v), str(1234.567), TFT.BLUE, font, 5, nowrap=True)
-    time.sleep_ms(1500)
+def showNetworks():
+    font = sysfont.sysfont
+    wlan = network.WLAN(network.STA_IF)
+    nets = wlan.scan()
+    tft.fill(TFT.BLACK)
+    tft.text((0, 0), 'Networks', TFT.RED, font, 1, nowrap=True)
+    x = font["Height"]
+    for net in nets:
+        x=  x + 1 + font["Height"]
+        ssid = str(net[0].decode("utf-8"))
+        if (ssid == ''):
+            ssid = 'No SSID'
+        tft.text((0, x),ssid , TFT.GREEN, font, 1, nowrap=True)
 
-def test_main():
-    tftprinttest(sysfont.sysfont)
-    time.sleep_ms(100)
+
+def displayTemp():
+    adcpin = 4
+    sensor = ADC(adcpin)
+    adc_value = sensor.read_u16()
+    volt = (3.3/65535)*adc_value
+    temperature = 27 - (volt - 0.706)/0.001721  
+
+    font = sysfont.sysfont
+    tft.fill(TFT.BLACK)
+    tft.text((0, 0), 'Temperature', TFT.RED, font, 1, nowrap=True)
+    x = font["Height"]
+    tft.text((0, x), 'Core: ' + str(temperature) + 'C', TFT.GREEN, font, 1, nowrap=True)
+
 
 if __name__ == "__main__":
     spi = SPI(0, baudrate=20000000, polarity=0, phase=0,
               sck=Pin(6), mosi=Pin(7), miso=None)
     tft=TFT(spi,11,10,12)
+    tft.rotation(1)
     tft.initr()
     tft.rgb(True)
-    test_main()
+    while True:
+        showNetworks()
+        time.sleep(5)
+        displayTemp()
+        time.sleep(5)
+
