83f6d1ec782de17a2546342446a382844cc53c25
aclem43 <aclemeur@gmail.com>
2025-01-30 20:04:33 +1000

    Dummy Data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from machine import SPI,Pin,ADC
 import network
 import time
 import _thread
-import math
+import random
 
 def showNetworks():
     font = sysfont.sysfont
@@ -37,6 +37,8 @@ def displayTemp():
 
 class SubieObd:
     def __init__(self,tft:TFT, widgets = False) -> None: 
+        self.version = '0.2'
+
         self.tft = tft
         self.starting = False
         self.mainpage = False
@@ -44,6 +46,9 @@ class SubieObd:
         self.widgets = []
         if widgets == False:
             self.defaultWidgets()
+        
+        self.data = {}
+        self.updatedData = False
 
         self.font = sysfont.sysfont
         self.fontHeight = self.font["Height"]
@@ -55,8 +60,6 @@ class SubieObd:
         self.connected = False
         self.sLock = _thread.allocate_lock()
 
-        self.version = '0.1'
-        pass
 
     def clearScreen(self):
         tft.fill(TFT.BLACK)
@@ -98,16 +101,18 @@ class SubieObd:
         self.connected = True
 
     def mainPageLoop(self):
-        self.displayData()
-        updatedData = False
+        self.displayWidgets()
+        self.updatedData = False
         while self.mainpage:
-            if updatedData != True:
+            self.updateData()
+            if self.updatedData != True:
                 time.sleep_ms(250)
                 continue
             self.displayData()
+            self.updatedData = False
             time.sleep_ms(250)
 
-    def displayData(self):
+    def displayWidgets(self):
         self.clearScreen()
         self.tft.text((0, 0), 'Subie OBD v' + self.version, TFT.RED, self.font, 1, nowrap=True)
 
@@ -117,16 +122,43 @@ class SubieObd:
             self.tft.text((0, widgetHeight), "No Widgets Set", TFT.RED, self.font, 2, nowrap=True)
             widgetHeight += self.fontHeight2 + 1
 
+        if (self.connected == False):
+            self.tft.text((0, widgetHeight), "Not Connected", TFT.RED, self.font, 2, nowrap=True)
+            widgetHeight += self.fontHeight2 + 1
+
+        for widget in self.widgets:
+            self.tft.text((0, widgetHeight), widget['title']+': ', TFT.GREEN, self.font, 2, nowrap=True)
+            widgetHeight += self.fontHeight2 + 1
+
+    def displayData(self):
+        self.tft.text((0, 0), 'Subie OBD v' + self.version, TFT.RED, self.font, 1, nowrap=True)
+
+        widgetHeight = self.fontHeight + 2
+
+        if (self.widgets == []):
+            self.tft.text((0, widgetHeight), "No Widgets Set", TFT.RED, self.font, 2, nowrap=True)
+            widgetHeight += self.fontHeight2 + 1
 
         if (self.connected == False):
             self.tft.text((0, widgetHeight), "Not Connected", TFT.RED, self.font, 2, nowrap=True)
             widgetHeight += self.fontHeight2 + 1
 
-
         for widget in self.widgets:
-            self.tft.text((0, widgetHeight), widget['title']+': 0', TFT.GREEN, self.font, 2, nowrap=True)
+            dat = 'Error'
+            try:
+                dat = self.data[widget['key']]
+            except KeyError:
+                dat = 'N/A'
+            x = (len(widget['title']) + 2) * self.font["Width"] * 2
+            self.tft.fillrect((x, widgetHeight), (160, self.fontHeight2), TFT.BLACK)
+            self.tft.text((x, widgetHeight),  str(dat) , TFT.GREEN, self.font, 2, nowrap=True)
             widgetHeight += self.fontHeight2 + 1
 
+        
+
+    def updateData(self):
+        self.data = {'speed':  random.randint(0,120), 'rpm': random.randint(0,6000), 'coolant': random.randint(0,140), 'voltage': random.randint(0,14)}
+        self.updatedData = True
 
     def defaultWidgets(self):
         self.addWidget('Speed', 'speed')
